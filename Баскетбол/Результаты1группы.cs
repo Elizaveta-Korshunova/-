be11fc8e0fccddf5afbaab2c3b875bbc6b3cf7eb
Elizaveta-Korshunova <83151653+Elizaveta-Korshunova@users.noreply.github.com>
2021-06-01 21:29:28 +0700

    Add files via upload

---
+++ Баскетбол/Результаты1группы.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Баскетбол
+{
+    public partial class Результаты1группы : Form
+    {
+        public void Вывод(Сборные[] array)
+        {
+            {
+                РезультатыГруппы_dataGridView.Columns.Add("НаименованиеСтраны", "Страна");
+                РезультатыГруппы_dataGridView.Columns.Add("Место", "Место");
+
+
+                РезультатыГруппы_dataGridView.Rows.Add(array.Length);
+                for (int i = 0; i < array.Length; i++)
+                {
+                    РезультатыГруппы_dataGridView["НаименованиеСтраны", i].Value = array[i].Страна;
+                    РезультатыГруппы_dataGridView["Место", i].Value = array[i].МестоГруппа;
+
+                }
+            }
+            Show();
+        }
+        
+        public Результаты1группы()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Баскетбол/Сборные.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Баскетбол
+{
+    public class Сборные
+    {
+        public string Страна;
+        public int ИндексРезультатов;
+        public int[] ЗаброшенныеМячи;
+        public int[] ПропущенныеМячи;
+        public int[] РазницаЗабИПроп;
+        public int Результат;
+        public int ЛучшаяРазница;
+        public int СуммаПроп;
+        public int МестоГруппа;
+        public int МестоИтог;
+
+
+        public static int СравнитьСБ(Сборные a, Сборные b)
+        {
+            if (a.Результат > b.Результат) // по очкам
+                return -1;
+            else if (a.Результат < b.Результат)
+                return 1;
+            else if (a.ЛучшаяРазница > b.ЛучшаяРазница) // по лучшей разнице пропущенных и заброшенных мячей
+                return -1;
+            else if (a.ЛучшаяРазница < b.ЛучшаяРазница)
+                return 1;
+            else if (a.СуммаПроп < b.СуммаПроп) // по наименьшему количеству пропущенных мячей
+                return -1;
+            else if (a.СуммаПроп > b.СуммаПроп)
+                return 1;
+            else if (a.ЗаброшенныеМячи[b.ИндексРезультатов] > b.ЗаброшенныеМячи[a.ИндексРезультатов]) // по результатам игр между собой
+                return -1;
+            else if (a.ЗаброшенныеМячи[b.ИндексРезультатов] < b.ЗаброшенныеМячи[a.ИндексРезультатов])
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
